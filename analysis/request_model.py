try to fix softmax function

analysis/request_model.py
@@ -32,7 +32,7 @@ args = argparser.parse_args()
 
 
 # Load the trained model
-model = AQUASSlidingBERT.from_pretrained(args.model, return_dict= False)
+model = AQUASSlidingBERT.from_pretrained(args.model)
 
 # Preprocess the specific text
 text = "Your specific text here"
@@ -49,9 +49,11 @@ print("text is preprocessed")
 
 
 output = model(input_ids=input_ids, attention_mask= attn_mask)
+output = torch.stack(output, dim=0)
+soft_output = torch.softmax(output)
 logits = output['logits']
 sigmoid_output =torch.sigmoid(logits)
-soft_output = torch.softmax(output)
+
 
 
 print('without softmax', sigmoid_output)